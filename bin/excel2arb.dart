6f576673b402f9e6e16b92f3dd2a2b628d74acd2
toelie <toelie.dev@gmail.com>
2023-08-20 15:33:06 +0700

    Support to run flutter gen-l10n with -g true/false flag

--- bin/excel2arb.dart
+++ bin/excel2arb.dart
@@ -1,27 +1,103 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:excel2arb/excel2arb.dart';
+import 'package:args/args.dart';
+
+const argExcelUrl = 'excel-url';
+const argLocalizationSheetName = 'sheet-name';
+const argOutputDirectory = 'output-directory';
+const argGenerateL10n = 'gen-l10n';
 
 void main(List<String> arguments) async {
-  final usage = 'Usage: excel2arb <url_to_excel_file> <sheet_name_to_parse> <path_to_output_directory>';
+  exitCode = 0; // presume success
+  final usage =
+      'Usage: excel2arb -u <excel-file-url> -s <sheet_name_to_parse> -o <path_to_arb_output_directory> -g <generate_l10n>';
   if (arguments.isEmpty) {
     print(usage);
+    exitCode = 1;
     return;
   }
 
-  final excelUrl = arguments.firstOrNull ?? '';
-  if (excelUrl.isEmpty) {
-    print('Excel file url is missing!');
-    print(usage);
-    return;
+  try {
+    final argParser = ArgParser()
+      ..addOption(
+        argExcelUrl,
+        abbr: 'u',
+        help: 'Excel file url',
+      )
+      ..addOption(
+        argLocalizationSheetName,
+        abbr: 's',
+        help: 'Sheet name to parse (default: Localization)',
+        defaultsTo: 'Localization',
+      )
+      ..addOption(
+        argOutputDirectory,
+        abbr: 'o',
+        help: 'Path to arb files output directory (default: current directory)',
+        defaultsTo: '.',
+      )
+      ..addFlag(
+        argGenerateL10n,
+        abbr: 'g',
+        help: 'Generate l10n files',
+        defaultsTo: false,
+        negatable: false,
+      );
+
+    final argResults = argParser.parse(arguments);
+    final excelUrl = argResults[argExcelUrl] ?? '';
+    if (excelUrl.isEmpty) {
+      print('Excel file url is missing!');
+      print(usage);
+      return;
+    }
+
+    final sheetName = argResults[argLocalizationSheetName] ?? 'Localization';
+    final outputDirectory = argResults[argOutputDirectory] ?? '.';
+
+    final converter = ExcelToArbGenerator(
+      excelUrl,
+      tempExcelPath: 'temp.xlsx',
+      outputArbDirectory: outputDirectory,
+      sheetName: sheetName,
+    );
+    await converter.convert();
+
+    final genL10n = argResults[argGenerateL10n];
+    if (genL10n) {
+      executeCommand('flutter gen-l10n');
+    }
+
+    exitCode = 0;
+  } catch (e) {
+    print('Error: $e');
+    exitCode = 2;
   }
+}
 
-  final sheetName = arguments.elementAtOrNull(1) ?? 'Localization';
-  final outputDirectory = arguments.elementAtOrNull(2) ?? '.';
+void executeCommand(String command) async {
+  print('Executing command: $command');
 
-  final converter = ExcelToArbGenerator(
-    excelUrl,
-    tempExcelPath: 'temp.xlsx',
-    outputArbDirectory: outputDirectory,
-    sheetName: sheetName,
+  final process = await Process.start(
+    command.split(' ')[0],
+    command.split(' ').sublist(1),
+    runInShell: true,
   );
-  converter.convert();
+
+  process.stdout.transform(utf8.decoder).listen((data) {
+    print(data);
+  });
+
+  process.stderr.transform(utf8.decoder).listen((data) {
+    print(data);
+  });
+
+  final exitCode = await process.exitCode;
+  if (exitCode == 0) {
+    print('Command executed successfully!');
+  } else {
+    print('Command failed with exit code: $exitCode');
+  }
 }
